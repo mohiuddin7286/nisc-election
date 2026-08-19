3f1b7d3d809c8be0286eeb8b1cf3c938a406dd30
Mohiuddin Ahmad <2410080026@klh.edu.in>
2026-08-19 17:52:04 +0530

    Provide fallback credentials in lib/supabase.ts for Vercel deployment build environment

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "https://owktquehybemngkwhzzz.supabase.co";
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93a3RxdWVoeWJlbW5na3doenp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY4MTQzNTIsImV4cCI6MjEwMjM5MDM1Mn0.nlqb28A9BHukIMkLZdEcHrEPzGWEBX4Tbo2nWgOyHaY";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
